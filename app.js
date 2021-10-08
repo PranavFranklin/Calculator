c1925186e465c4cfc988fbad4c991c1e4c25b371
pranav <meejuripranav@gmai.com>
2021-10-08 10:20:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const users = require("./users")
+
+const app = express()
+
+app.use(bodyparser.urlencoded({extended:true}))
+
+
+app.get('', (req,res)=>{
+    res.sendFile(__dirname + "/index.html")
+
+})
+
+app.post('/',(req,res)=>{
+    const a = Number(req.body.FirstNumber)
+    const b = Number(req.body.SecondNumber)
+    const add = a + b
+    //const sub = fsub()
+    res.send("The Value is" + " " + add)
+   
+})
+
+
+app.listen(3000, (res)=>{
+        console.log('Server is running on port 3000')
+})
